eb8704406d728f993888f3bc68c4f596a0e5392e
cdennis27 <113468121+cdennis27@users.noreply.github.com>
2024-01-27 17:31:21 -0500

    test email sending system

--- js/script.js
+++ js/script.js
@@ -17,3 +17,35 @@ console.log(menuButton.classList);
         toggleButton.setAttribute('aria-expanded', 'false');
     }
 });
+
+function sendMail(){
+    let params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value
+    }
+    
+    emailjs.send("service_uvypnm9", "template_3mb35ve", params).then(alert("Email Sent!!!"))
+    
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
