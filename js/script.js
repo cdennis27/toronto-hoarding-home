3df9eada9791712395b2ae86340ef7f7aa533729
cdennis27 <113468121+cdennis27@users.noreply.github.com>
2024-01-28 13:44:05 -0500

    added callbutton css and changed some photos from soto

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbar = document.getElementsByClassName('navbar')[0]
 const menuButton = document.querySelector('.menuButton')
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const subject = document.getElementById('subject')
+const message = document.getElementById('message')
+const errorElement = document.getElementById('error')
 
 toggleButton.addEventListener('click', () => {
     navbar.classList.toggle('active');
     toggleButton.classList.toggle('close');
     menuButton.classList.toggle('close');
-console.log(menuButton.classList);
+    console.log(menuButton.classList);
     const isOpened = menuButton.getAttribute('aria-expanded');
     if (isOpened === 'false') {
         menuButton.setAttribute('aria-expanded', 'true');
@@ -18,16 +23,38 @@ console.log(menuButton.classList);
     }
 });
 
-function sendMail(){
-    let params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value
+function sendMail() {
+
+    console.log("email info" + name.value, email.value, subject.value, message.value)
+
+    if (email.value === "" || name.value === "" || subject.value === "" || message.value === "") {
+        errorElement.innerText = ("Please fill out all fields")
+        return
     }
-    
-    emailjs.send("service_uvypnm9", "template_3mb35ve", params).then(alert("Email Sent!!!"))
-    
+
+    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))) {
+        console.log("email is invalid")
+
+        errorElement.innerText = ("Please enter a valid email address")
+        alert("Please enter a valid email address, form will be reset.")
+        return
+    }
+
+
+
+
+
+    let params = {
+        name: name.value,
+        email: email.value,
+        subject: subject.value,
+        message: message.value
+    }
+    console.log(params)
+    console.log(params.name, params.email, params.subject)
+
+    //emailjs.send("service_uvypnm9", "template_3mb35ve", params).then(alert("Email Sent!!!"))
+    console.log(params)
 
 
 }
